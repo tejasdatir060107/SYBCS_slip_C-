e8ff18d0587ff97fc2439d8079a412285682856d
Tejas Datir <tejasdatir2021@gmail.com>
2025-10-04 23:31:27 +0530

    13slipQ1.cpp

---
+++ 13slipQ1.cpp
@@ -0,0 +1,54 @@
+/* Write a C++ program to create a class Distance with data members feet and inches. Write 
+member functions for the following: 
+a. To accept distance 
+b. To display distance 
+c. To add two distance objects 
+(Use object as a function argument and function returning object).    */
+
+
+using namespace std;
+#include<iostream>
+
+class distanc
+{
+    public:
+    int feet,inches;
+    void accept()
+    {
+        cout<<"enter distance in feet and inches:";
+        cin>>feet>>inches;
+    }
+    void disp()
+    {
+        while(inches>=12)
+        {
+            feet++;
+            inches=inches-12;
+        }
+        cout<<feet<<" feet "<<inches<<" inches "<<endl;
+    }
+    friend distanc add(distanc ob,distanc ob1);
+};
+    
+        distanc add(distanc ob,distanc ob1)
+        {
+            distanc ob2;
+            ob2.feet=ob.feet+ob1.feet;
+            ob2.inches=ob.feet+ob1.inches;
+            return ob2;
+        }
+
+        
+int main()
+{
+    distanc ob,ob1,ob2;
+    ob.accept();
+    ob1.accept();
+
+    ob.disp();
+    ob1.disp();
+    ob2=add(ob,ob1);
+    ob2.disp();
+
+}
+
