5ac2f2ffab34def62ddb33b61fbedc65b29d9089
Tejas Datir <tejasdatir2021@gmail.com>
2025-10-04 23:42:49 +0530

    14slipQ2.cpp

---
+++ 14slipQ2.cpp
@@ -0,0 +1,45 @@
+/*Write a C++ program to print the following pattern. 
+ 
+              * 
+            * * 
+          * * * 
+        * * * * 
+        */ 
+
+
+   /*Write a C++ program to print the following pattern. 
+ 
+              * 
+            * * 
+          * * * 
+        * * * * 
+        */ 
+
+
+   #include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter limit: ";
+    cin>>n;
+
+    for(int i=1;i<=n;i++) 
+	{
+        for(int j=1;j<=n-i;j++)
+		 {
+            cout<<" ";
+         }
+        for(int k=1;k<=i;k++) 
+		{
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+
+
+
+
