cf4e4a2812cb116a1bb10d56ec6bf5fa52b9d248
Tejas Datir <tejasdatir2021@gmail.com>
2025-10-04 23:57:58 +0530

    2slipQ2.cpp

--- 2slipQ2.cpp
+++ 2slipQ2.cpp
@@ -1,9 +1,7 @@
- /*Write a C++ program to define power function to calculate x^y. (Use default value  
-as 2   for y).*/
+ /*Write a C++ program to define power function to calculate x^y. (Use default value  as 2   for y).*/
 
 #include<iostream>
 using namespace std;
-
 
 void power(int x,int y=2)
 {
