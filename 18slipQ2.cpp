b2eb9715820ebced2a62d01944111225104414ca
Tejas Datir <tejasdatir2021@gmail.com>
2025-10-05 00:06:39 +0530

    18slipQ2.cpp

---
+++ 18slipQ2.cpp
@@ -0,0 +1,38 @@
+/*Write a C++ program to accept ‘n’ float numbers, store them in an array and print the 
+alternate elements of an array. (Use dynamic memory allocation). */
+
+#include<iostream>
+using namespace std;
+
+class array
+{
+    public:
+    float *a;
+    int n;
+    void accept()
+    {
+        cout<<"enter limit:";
+        cin>>n;
+        a=new float[n];
+        cout<<"enter n number in array:";
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+    }
+    void display()
+    {
+        cout<<" alternative array:";
+        for(int i=0;i<n;i=i+2)
+        {
+            cout<<a[i]<<"  ";
+        }
+        delete []a;
+    }
+};
+
+int main()
+{
+    array ob;
+    ob.accept();
+    ob.display();
+
+}
